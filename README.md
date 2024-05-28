a73205d8eeb69c60fe26a3d5a6589a430f0fd3e8
Hiroki Ueno <rockyueno@gmail.com>
2024-05-28 16:23:20 -0700

    First commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Getting started
+
+## Installation
+
+```js
+npm install @rockyueno/my-map-function
+```
+
+## How to use
+
+```js
+const myPackage = require("@rockyueno0223/my-map-function");
+const result = myPackage.myMap(array);
+```
---
+++ index.js
@@ -0,0 +1,13 @@
+function myMap(cbFn) {
+  return function (array) {
+    const result = [];
+    array.forEach((element, index) => {
+      result.push(cbFn(element, index));
+    });
+    return result;
+  }
+}
+
+console.log(`myMap`);
+
+module.exports = myMap;
